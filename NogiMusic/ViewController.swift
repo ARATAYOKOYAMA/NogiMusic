a0d6870de5950c512db1fc6fa78c34064d225b6a
ARATAYOKOYAMA <bob2525@hotmail.co.jp>
2018-04-15 22:34:54 +0900

    first commit

---
+++ NogiMusic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NogiMusic
+//
+//  Created by 横山　新 on 2018/04/15.
+//  Copyright © 2018年 横山　新. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
